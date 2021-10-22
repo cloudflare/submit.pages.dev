973c4a879db1a40c5c41ccbba1b752e891544b1f
Luke Edwards <luke.edwards05@gmail.com>
2021-10-22 10:09:48 -0700

    fix: always make `next` response mutable

--- functions/_middleware_.js
+++ functions/_middleware_.js
@@ -1,5 +1,6 @@
 export const onRequestGet = async ({ next }) => {
-  const response = await next()
+  let response = await next()
+	response = new Response(response.body, response);
   response.headers.set('X-Hello', 'Hello from Plinko Middleware!')
   return response
 }
