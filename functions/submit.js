c27fca9e0719504aea7f8b02b4a442ce0325718b
Luke Edwards <luke.edwards05@gmail.com>
2021-10-21 21:31:48 -0700

    fix(api): allow multiple values per key

--- functions/submit.js
+++ functions/submit.js
@@ -3,9 +3,21 @@
  */
 export async function onRequestPost({ request }) {
 	try {
-		let raw = await request.formData();
-		let data = Object.fromEntries(raw);
-		let pretty = JSON.stringify(data, null, 2);
+		let input = await request.formData();
+
+		// Convert FormData to JSON
+		// NOTE: Allows mutliple values per key
+		let tmp, output = {};
+		for (let [key, value] of input) {
+			tmp = output[key];
+			if (tmp === undefined) {
+				output[key] = value;
+			} else {
+				output[key] = [].concat(tmp, value);
+			}
+		}
+
+		let pretty = JSON.stringify(output, null, 2);
 		return new Response(pretty, {
 			headers: {
 				'Content-Type': 'application/json;charset=utf-8'
